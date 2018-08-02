11a37d9763879404267c4e1e18cc31770a2b3e12
ganjiron <ssuckies@naver.com>
2018-08-02 14:55:38 +0900

    8/2 오전

--- ch31/ch31.py
+++ ch31/ch31.py
@@ -6,12 +6,12 @@ print(a , type(a))
 a = np.array([[1,2,3],[4,5,6]] , float)
 print(a[0,0] , a[1,1])
 
-print( np.zeros((3,3)))
-print(np.ones((2,3)))
-print(np.full((3,3),8))
-print(np.eye(3))
-print(np.random.random((3,3)))
-print(np.random.rand(3,3))
+# print( np.zeros((3,3)))
+# print(np.ones((2,3)))
+# print(np.full((3,3),8))
+# print(np.eye(3))
+# print(np.random.random((3,3)))
+# print(np.random.rand(3,3))
 
 
 if __name__ == '__main__':
