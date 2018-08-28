31325a8a2206b3bfcf5d150881b72c895e591e4a
ganjiron <ssuckies@naver.com>
2018-08-28 12:18:35 +0900

    8/28
    Signed-off-by: ganjiron <ssuckies@naver.com>

--- HW/hw31.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-def q1():
-    a1_1 = np.array([[1,2],[3,4]])
-    a1_2 = np.array([[1, 2], [3, 4],[5,6]])
-
-if __name__ == '__main__':
-    pass
--- ch15/test1.py
+++
@@ -1,9 +0,0 @@
-class test:
-    age = 23
-    name = 'Adam'
-
-t = test()
-print(t.name , t.age)
-print(getattr(test , 'sex' , 'Male'))
-print(test.sex)
-# print(getattr(test , 'sex' ))
