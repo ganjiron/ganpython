96cb924d15bd4e1e0ecb78f819f72a694ed5d1cb
ganjiron <ssuckies@naver.com>
2018-08-28 12:14:25 +0900

    8/27
    Signed-off-by: ganjiron <ssuckies@naver.com>

--- PythonClass/c34.py
+++ PythonClass/c34.py
@@ -2,10 +2,18 @@
 # diabetes = datasets.load_diabetes()
 # print(diabetes)
 
-from matplotlib.colors import ListedColormap
-from sklearn import neighbors, datasets
+from sklearn import datasets
 n_neighbors = 15
 iris = datasets.load_iris()
 X = iris.data[:,:2]
 y = iris.target
 h = .02
+
+from sklearn.datasets import load_iris
+from sklearn import tree
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
+print(clf)
